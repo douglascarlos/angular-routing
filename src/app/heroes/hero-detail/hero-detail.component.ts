b4ac8125899318a0c6e31653ffe5d6f19c3275c6
douglas <dnevescarlos@gmail.com>
2018-01-24 10:44:35 -0200

    add route parameter for selected hero

--- src/app/heroes/hero-detail/hero-detail.component.ts
+++ src/app/heroes/hero-detail/hero-detail.component.ts
@@ -1,11 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+import { Observable } from 'rxjs/Observable';
+
 import { Hero } from '../hero';
+import { Route } from '@angular/router/src/config';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent {
-  @Input() hero: Hero;
+export class HeroDetailComponent implements OnInit {
+
+  hero$: Observable<Hero>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private heroService: HeroService
+  ) {}
+
+  ngOnInit() {
+    this.hero$ = this.route.paramMap.switchMap(
+      (parameters: ParamMap) => this.heroService.getHero(parameters.get('id'))
+    );
+  }
+
+  gotoHeroes(hero: Hero) {
+    let heroId = hero ? hero.id : hero;
+    this.router.navigate(['/heroes', { id: heroId, foo: 'foo' }]);
+  }
+
 }
--- src/app/heroes/hero-list/hero-list.component.ts
+++ src/app/heroes/hero-list/hero-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+import { Observable } from 'rxjs/Observable';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -10,22 +13,29 @@ import { HeroService } from '../hero.service';
 })
 export class HeroListComponent implements OnInit {
 
-  title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
+  heroes$: Observable<Hero[]>;
+  selectedId: number;
 
-  constructor(private heroService: HeroService) { }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroes$ = this.route.paramMap
+      .switchMap((params: ParamMap) => {
+        // (+) before `params.get()` turns the string into a number
+        this.selectedId = +params.get('id');
+        return this.heroService.getHeroes();
+      });
+  }
+
   onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+    // this.selectedHero = hero;
   }
 
 }
