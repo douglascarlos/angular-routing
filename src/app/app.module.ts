c81f21382dd5727eaf1bee124a1174df5a4cf50a
douglas <dnevescarlos@gmail.com>
2018-01-25 11:46:42 -0200

    inspect the router's configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }    from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router }    from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -36,4 +37,10 @@ import { DialogService } from './dialog.service';
     AppComponent
   ]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(router: Router) {
+    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
+
+}
